a5bf1baa8cb08c2c32419711317bacdea68e4e7f
Isaac-Lee <isaac.lee.3412@gmail.com>
2022-07-04 16:49:08 +0900

    [solve] deep readonly

---
+++ challenge/medium/Deep-Readonly.ts
@@ -0,0 +1,105 @@
+/*
+  9 - Deep Readonly
+  -------
+  by Anthony Fu (@antfu) #medium #readonly #object-keys #deep
+  
+  ### Question
+  
+  Implement a generic `DeepReadonly<T>` which make every parameter of an object - and its sub-objects recursively - readonly.
+  
+  You can assume that we are only dealing with Objects in this challenge. Arrays, Functions, Classes and so on are no need to take into consideration. However, you can still challenge your self by covering different cases as many as possible.
+  
+  For example
+  
+  ```ts
+  type X = { 
+    x: { 
+      a: 1
+      b: 'hi'
+    }
+    y: 'hey'
+  }
+  
+  type Expected = { 
+    readonly x: { 
+      readonly a: 1
+      readonly b: 'hi'
+    }
+    readonly y: 'hey' 
+  }
+  
+  type Todo = DeepReadonly<X> // should be same as `Expected`
+  ```
+  
+  > View on GitHub: https://tsch.js.org/9
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+type DeepReadonly<T> = keyof T extends never ? T : { readonly [k in keyof T]: DeepReadonly<T[k]> };
+// never는 terminal의 의미로도 사용이 가능한듯
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<DeepReadonly<X>, Expected>>,
+]
+
+type X = {
+  a: () => 22
+  b: string
+  c: {
+    d: boolean
+    e: {
+      g: {
+        h: {
+          i: true
+          j: 'string'
+        }
+        k: 'hello'
+      }
+      l: [
+        'hi',
+        {
+          m: ['hey']
+        },
+      ]
+    }
+  }
+}
+
+type Expected = {
+  readonly a: () => 22
+  readonly b: string
+  readonly c: {
+    readonly d: boolean
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true
+          readonly j: 'string'
+        }
+        readonly k: 'hello'
+      }
+      readonly l: readonly [
+        'hi',
+        {
+          readonly m: readonly ['hey']
+        },
+      ]
+    }
+  }
+}
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/9/answer
+  > View solutions: https://tsch.js.org/9/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
